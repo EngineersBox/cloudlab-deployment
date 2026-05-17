2681fff7a24c62550293c34d4ce3de3fdad98f3b
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2026-05-17 13:44:15 +1000

    Added usage details for AWS provisioning
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- aws_prov/templates/variables.tf
+++ aws_prov/templates/variables.tf
@@ -1,24 +1,29 @@
 variable "private_key_name" {
 	type    = "string"
+  # Name of private key file to use on the AWS instances
 	default = ""
 }
 
 variable "private_key_location" {
 	type    = "string"
+  # Path on your local machine to your SSH private key
 	default = ""
 }
 
 variable "cluster_ami" {
   type = "string"
+  # Base OS AMI to load onto the nodes
   default = "REQUIRED"
 }
 
 variable "aws_region" {
   type = "string"
+  # Where should we provisioning the instances
   default = "us_east_1"
 }
 
 variable "instance_type" {
   type = "string"
+  # What AWS instance size should we use
   default = "t3.small"
 }
